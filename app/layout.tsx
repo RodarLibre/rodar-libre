5aade436d70d24f77113c3c71a46492e7a51d4e7
Juan Roa <hello@juanroa.dev>
2026-09-16 08:58:31 -0500

    feat: update favicon assets and add root icon to layout metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export const metadata: Metadata = {
   description: "Rueda libre, vive la ruta, cuida tu vehículo. Síguenos en @rodarlibre.co",
   icons: {
     icon: [
+      { url: "/favicon.ico" },
       { url: "/favicon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
